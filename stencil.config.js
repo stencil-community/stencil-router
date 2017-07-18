a2d469b3e7527007c80f989982118104df728edd
Max Lynch <max@drifty.com>
2017-07-18 10:09:43 -0500

    Update stencil.config.js

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,5 @@
 exports.config = {
   bundles: [
-    { components: ['stencil-site', 'site-header', 'landing-page', 'docs-page', 'demos-page'] },
     { components: ['stencil-router', 'stencil-route', 'stencil-route-link'] }
   ]
 };

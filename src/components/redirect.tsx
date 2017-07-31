529a649b6e8a37770be20d9af47f65f3c5488d6a
jgw96 <jgw9617@gmail.com>
2017-07-31 16:27:54 -0500

    chore(): fix derp and remove h import

--- src/components/redirect.tsx
+++ src/components/redirect.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop } from '@stencil/core';
 
 @Component({
   tag: 'stencil-router-redirect'
--- src/components/route.tsx
+++ src/components/route.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, State, Element } from '@stencil/core';
 
 /**
   * @name Route
@@ -6,43 +6,84 @@ import { Component, Prop } from '@stencil/core';
   * @description
  */
 @Component({
-  tag: 'stencil-route-link'
+  tag: 'stencil-route'
 })
-export class RouteLink {
+export class Route {
+  @Element() el: any;
+
+  @State() routerInstance: any;
+
   @Prop() url: string;
 
-  @Prop() custom: boolean = false;
+  @Prop() component: string;
+
+  @Prop() componentProps: any = {};
+
+  @Prop() exact: boolean = false;
 
   // The instance of the router
   @Prop() router: any;
 
-  handleClick(e) {
-    console.log('Route link click', e);
-    const router = document.querySelector(this.router);
-    if(!router) {
-      console.warn('<stencil-route-link> wasn\'t passed an instance of the router as the "router" prop!');
-      return;
-    }
+  //@Prop() match: any;
+  @State() match: any = {};
 
-    router.navigateTo(this.url);
+  componentWillLoad() {
+    setTimeout(() => {
+      console.log(document.querySelector(this.router));
+      const routerElement = document.querySelector(this.router);
 
-    //Uncomment once https://github.com/ionic-team/stencil/issues/58 is fixed
-    //e.preventDefault();
+      if (routerElement) {
+        setTimeout(() => {
+          console.log(routerElement);
+          this.routerInstance = routerElement;
+        })
+      }
+
+      routerElement.addEventListener('stencilRouterLoaded', (e) => {
+        console.log('got fired');
+        // this.routerInstance = this.el
+        // this.routerInstance = routerElement.$instance;
+      })
+
+      routerElement.addEventListener('stencilRouterNavigation', (e) => {
+        console.log('i got fired', e.detail);
+        //console.log(`<stencil-route> for ${this.path} got nav event`, e.detail);
+        this.match = e.detail;
+      })
+    })
+    /*const routerElement = document.querySelector(this.router)
+    console.log(routerElement);*/
   }
 
   render() {
-    if(this.custom) {
-      return (
-        <span onClick={this.handleClick.bind(this)}>
-          <slot></slot>
-        </span>
-      );
+    console.log('render called');
+    if (!this.routerInstance) {
+      return null;
+    }
+
+    //console.log(`<stencil-route> for ${this.path} rendering`);
+    this.match.url = this.routerInstance.match().url;
+    const match = this.match;
+    const ChildComponent = this.component;
+    console.log('this.component', this.component);
+
+    // Check if this route is in the matching URL (for example, a parent route)
+    // const isInPath = this.match.url.indexOf(this.url) == 0;
+
+    // const matches = this.exact ? match.url == this.url : isInPath;
+    const matches = match.url === this.url;
+    console.log('is exact', this.exact);
+    console.log('match.url', match.url);
+    console.log('this.url', this.url);
+
+    console.log(`\tDoes ${match.url} match our path ${this.url}?`, matches)
+
+    if (matches) {
+      console.log('childComponent', ChildComponent);
+      console.log(`  <ion-route> Rendering route ${this.url}`, this.router, match);
+      return (<ChildComponent props={this.componentProps} />);
     } else {
-      return (
-        <a onClick={this.handleClick.bind(this)}>
-          <slot></slot>
-        </a>
-      )
+      return <span></span>;
     }
   }
 }

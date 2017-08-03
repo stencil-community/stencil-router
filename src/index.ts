0b6a1df57617fcff5ec3af3bd4cf47d4f9645330
jgw96 <jgw9617@gmail.com>
2017-08-03 13:54:26 -0500

    chore(): add index file and update file structure

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Router } from './components/router/router';
+import { Route } from './components/route/route';
+import { Redirect } from './components/redirect/redirect';
+import { RouteLink } from './components/route-link/route-link';
+
+export {
+  Router,
+  Route,
+  Redirect,
+  RouteLink
+}

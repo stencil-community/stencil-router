00e25469c6c24a05e400431e10e5954e69bdc7f4
Josh Thomas <josh@ionic.io>
2017-09-01 10:04:51 -0500

    chore(): updated tests.

--- src/components/test-page/test-page.tsx
+++ src/components/test-page/test-page.tsx
@@ -8,20 +8,37 @@ export class TestPage {
   @Prop() pages: string[];
 
   render() {
-    debugger;
     return [
       <span>Demo 3 Test Page<br/></span>,
       <stencil-route url="/demo3/page1" exact={true} routeRender={
         (props: { [key: string]: any}) => {
           console.log(props);
-          return <span>rendering /demo3/page1</span>
+          return [
+            <a href="#" onClick={(e) => {
+              e.preventDefault();
+              props.history.push('/demo3/page2', { 'blue': 'blue' });
+            }}>
+              History push to /demo3/page2
+            </a>,
+            <br/>,
+            <span>rendering /demo3/page1</span>
+          ];
         }
       }></stencil-route>,
 
       <stencil-route url="/demo3/page2" exact={true} routeRender={
         (props: { [key: string]: any}) => {
           console.log(props);
-          return <span>rendering /demo3/page2</span>
+          return [
+            <a href="#" onClick={(e) => {
+              e.preventDefault();
+              props.history.push('/demo3/page1', { 'red': 'red' });
+            }}>
+              History push to /demo3/page1
+            </a>,
+            <br/>,
+            <span>rendering /demo3/page2</span>
+          ];
         }
       }></stencil-route>
     ];

76a2b40d7b7c93d0eb7db493914f57f981108408
Josh Thomas <jthoms1@gmail.com>
2018-05-09 13:03:20 -0500

    fix(): use stencil provided functional component utils for switch.

--- src/components/switch/switch.tsx
+++ src/components/switch/switch.tsx
@@ -1,20 +1,50 @@
-import { VNode } from "@stencil/core/dist/declarations";
 import uuidv4 from '../../utils/uuid';
 
-export const RouterSwitch = ({ children, scrollTopOffset }: { [key: string]: any}) => {
+export interface VNode {
+  vtag?: string | number | Function;
+  vkey?: string | number;
+  vtext?: string;
+  vchildren?: VNode[];
+  vattrs?: any;
+  vname?: string;
+  ishost?: boolean;
+  isSlotFallback?: boolean;
+  isSlotReference?: boolean;
+}
+export interface ComponentProps {
+  children?: any[];
+  key?: string | number | any;
+}
+export interface FunctionalUtilities {
+  getAttributes: (vnode: VNode) => any;
+  replaceAttributes: (vnode: VNode, attributes: any) => void;
+}
+export interface FunctionalComponent<PropsType> {
+  (props?: PropsType & ComponentProps, utils?: FunctionalUtilities): VNode;
+}
 
-  let groupId: string;
+export interface ComponentProps {
+  scrollTopOffset?: number;
+}
+
+export const RouterSwitch: FunctionalComponent<ComponentProps> = ({ children, scrollTopOffset }, util) => {
+
+  let group: string;
   if (window.crypto) {
-    groupId = uuidv4();
+    group = uuidv4();
   } else {
-    groupId = (Math.random() * 10e16).toString();
+    group = (Math.random() * 10e16).toString();
   }
 
   const chil = children
-    .map((child: VNode, index: number) => {
-      child.vattrs.scrollTopOffset = scrollTopOffset;
-      child.vattrs.group = groupId;
-      child.vattrs.groupIndex = index;
+    .map((child: VNode, groupIndex: number) => {
+      const currentAttributes = util.getAttributes(child);
+      util.replaceAttributes(child, {
+        ...currentAttributes,
+        scrollTopOffset,
+        group,
+        groupIndex
+      });
       return child;
     });
 

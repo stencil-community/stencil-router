1f121ffd58c9c343eaa80769e698fd805fcbf1e0
shallurohilla <shallu.rh@gmail.com>
2018-08-02 23:19:05 +0530

    Add accessibility params to stencil-router-link (#61)

--- src/components/route-link/route-link.tsx
+++ src/components/route-link/route-link.tsx
@@ -23,7 +23,7 @@ export class RouteLink {
   @Prop() exact: boolean = false;
   @Prop() strict: boolean = true;
 
-  /**
+ /**
    *  Custom tag to use instead of an anchor
    */
   @Prop() custom: string = 'a';
@@ -36,8 +36,15 @@ export class RouteLink {
   @Prop() history: RouterHistory;
   @Prop() location: LocationSegments;
   @Prop() root: string;
+  
+  @Prop() ariaHaspopup: string;
+  @Prop() id: string;
+  @Prop() ariaPosinset: string;
+  @Prop() ariaSetsize: number;
+  @Prop() ariaLabel: string;
 
   @State() match: MatchResults | null = null;
+  
 
   componentWillLoad() {
     this.computeMatch();
@@ -92,8 +99,13 @@ export class RouteLink {
         href: this.url,
         title: this.anchorTitle,
         role: this.anchorRole,
-        tabindex: this.anchorTabIndex
-      }
+        tabindex: this.anchorTabIndex,
+        'aria-haspopup': this.ariaHaspopup,
+        id: this.id,
+        'aria-posinset': this.ariaPosinset,
+        'aria-setsize': this.ariaSetsize,
+        'aria-label': this.ariaLabel
+       }
     }
     return (
       <this.custom {...anchorAttributes}>

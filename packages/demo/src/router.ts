a34bd50ac22ef8e33b5423ba86aaab3c59e108cc
Dominik Pieper <dominik@pieper.io>
2022-07-26 11:54:02 +0200

    chore: move router v2 to new community schema, use lerna and update dependencies

---
+++ packages/demo/src/router.ts
@@ -0,0 +1,2 @@
+import { createStaticRouter } from '@stencil-community/router';
+export const Router = createStaticRouter();
--- test-app/src/router.ts
+++
@@ -1,2 +0,0 @@
-import { createStaticRouter } from 'stencil-router-v2';
-export const Router = createStaticRouter();
